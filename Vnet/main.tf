1eabe18651b9fdd988bd12979e23e296b90df75f
David Schick <alufdavid@gmail.com>
2023-11-26 13:45:39 +0200

    change 2 for poc

--- Vnet/main.tf
+++ Vnet/main.tf
@@ -1,7 +1,8 @@
 module "greenfin-tfc-lab-tf-code-modules" {
   source  = "app.terraform.io/cloud-castles/greenfin-tfc-lab-tf-code-modules/azurerm"
   version = "1.0.0"
-  environment  = "dev"
+  # insert required variables here
 }
 
 #test
+
