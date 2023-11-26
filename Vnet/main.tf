7dc0fd0ff0fe47d82d1f345f2438502546a3394c
David Schick <alufdavid@gmail.com>
2023-11-26 13:59:37 +0200

    change module name

--- Vnet/main.tf
+++ Vnet/main.tf
@@ -1,4 +1,4 @@
-module "greenfin-tfc-lab-tf-code-modules" {
+module "vnet" {
   source  = "app.terraform.io/cloud-castles/greenfin-tfc-lab-tf-code-modules/azurerm"
   version = "1.0.0"
   environment  = "dev"
