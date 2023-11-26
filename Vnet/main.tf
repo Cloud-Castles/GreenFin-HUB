399abc25546799739b4e81b9ad265eb52818b24f
David Schick <alufdavid@gmail.com>
2023-11-26 13:58:48 +0200

    poc check

--- Vnet/main.tf
+++ Vnet/main.tf
@@ -1,8 +1,5 @@
 module "greenfin-tfc-lab-tf-code-modules" {
   source  = "app.terraform.io/cloud-castles/greenfin-tfc-lab-tf-code-modules/azurerm"
   version = "1.0.0"
-  # insert required variables here
+  environment  = "dev"
 }
-
-#test
-
--- Vnet/outputs.tf
+++ Vnet/outputs.tf
@@ -1,14 +1,14 @@
 output "vnet_id" {
   description = "List of vnet id"
-  value = module.greenfin-tfc-lab-tf-code-modules.vnet_id
+  value = module.vnet.vnet_id
 }
 
 output "subnet_ids" {
   description = "List of subnet id's"
-  value = module.greenfin-tfc-lab-tf-code-modules.subnet_ids
+  value = module.vnet.subnet_ids
 }
 
 output "network_interface_ip" {
   description = "List of nic ip's"
-  value = module.greenfin-tfc-lab-tf-code-modules.network_interface_ip
+  value = module.vnet.network_interface_ip
 }
