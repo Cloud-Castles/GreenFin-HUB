6d96f43bfeb10a5480c2186c6db2639cc3464404
David Schick <alufdavid@gmail.com>
2023-11-26 14:06:29 +0200

    change path of source

--- Vnet/main.tf
+++ Vnet/main.tf
@@ -1,5 +1,6 @@
 module "vnet" {
-  source  = "app.terraform.io/cloud-castles/greenfin-tfc-lab-tf-code-modules/azurerm"
+  source  = "app.terraform.io/cloud-castles/vnet/azurerm"
   version = "2.0.0"
-  environment  = "dev"
+  # insert required variables here
 }
+
