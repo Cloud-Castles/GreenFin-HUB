6d8f999465d63e06a8168c8c732d939b179d5349
David Schick <alufdavid@gmail.com>
2023-11-27 18:15:56 +0200

    test14

--- Vnet/main.tf
+++ Vnet/main.tf
@@ -1,21 +1,22 @@
-data "external" "vnet_settings" {
-  program = ["cat", "./configuration.json"]
-}
-
-locals {
-  vnet_settings = jsondecode(data.external.vnet_settings.result)
-}
-
 # locals {
-#   vnet_settings = jsondecode(file("./config/configuration.json"))
+#   vnet_settings = jsondecode(file("./configuration.json"))
+# }
+
+# module "vnet" {
+#   source  = "app.terraform.io/cloud-castles/vnet/azurerm"
+#   version = "1.0.0"
+#   resource_group = local.vnet_settings.resource_group
+#   location = local.vnet_settings.location
+#   vnet_name = local.vnet_settings.vnet_name
+#   address_space = local.vnet_settings.address_space
 # }
 
 module "vnet" {
   source  = "app.terraform.io/cloud-castles/vnet/azurerm"
   version = "1.0.0"
-  resource_group = local.vnet_settings.resource_group
-  location = local.vnet_settings.location
-  vnet_name = local.vnet_settings.vnet_name
-  address_space = local.vnet_settings.address_space
+  resource_group = "hub-dev-rg"
+  location = "westeurope"
+  vnet_name = "hub-dev-vnet"
+  address_space = ["10.0.0.0/16"]
 }
 
