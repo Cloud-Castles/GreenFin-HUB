d88db55cd48da19f897ad4ba7fd47e0208f14f6c
David Schick <alufdavid@gmail.com>
2023-11-27 18:06:50 +0200

    test11

--- Vnet/main.tf
+++ Vnet/main.tf
@@ -1,5 +1,5 @@
 locals {
-  vnet_settings = jsondecode(file("${path.module}/configuration.json"))
+  vnet_settings = jsondecode(file("${path.module}/config/configuration.json"))
 }
 
 module "vnet" {
