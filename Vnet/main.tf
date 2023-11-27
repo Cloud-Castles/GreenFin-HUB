6ac6c22c8474cebd70eb0e78bd417fbdc86de2b3
David Schick <alufdavid@gmail.com>
2023-11-27 17:46:59 +0200

    test

--- Vnet/main.tf
+++ Vnet/main.tf
@@ -3,8 +3,8 @@ locals {
 }
 
 module "vnet" {
-  source         = "app.terraform.io/cloud-castles/vnet/azurerm"
-  version        = "2.0.0"
+  source  = "app.terraform.io/cloud-castles/vnet/azurerm"
+  version = "1.0.0"
   resource_group = local.vnet_settings.resource_group
   location = local.vnet_settings.location
   vnet_name = local.vnet_settings.vnet_name
