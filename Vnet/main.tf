ab59b84b69621c54227a0dc3b60f98da88a10681
David Schick <alufdavid@gmail.com>
2023-11-27 17:45:52 +0200

    change file structure

--- Vnet/main.tf
+++ Vnet/main.tf
@@ -1,7 +1,13 @@
+locals {
+  vnet_settings = jsondecode("./config/configuration.json")
+}
+
 module "vnet" {
-  source  = "app.terraform.io/cloud-castles/vnet/azurerm"
-  version = "2.0.0"
-  # insert required variables here
-  environment  = "dev"
+  source         = "app.terraform.io/cloud-castles/vnet/azurerm"
+  version        = "2.0.0"
+  resource_group = local.vnet_settings.resource_group
+  location = local.vnet_settings.location
+  vnet_name = local.vnet_settings.vnet_name
+  address_space = local.vnet_settings.address_space
 }
 
