523bf8e57d74c966bc97e39c11c90f476b535a82
David Schick <alufdavid@gmail.com>
2023-12-04 07:53:39 +0200

    change module to network and change and add vars

--- Vnet/main.tf
+++ Vnet/main.tf
@@ -1,13 +1,13 @@
 locals {
-  vnet_settings = jsondecode(file("./configuration.json"))
+  netowrk_settings = jsondecode(file("./configuration.json"))
 }
 
-module "vnet" {
-  source  = "app.terraform.io/cloud-castles/vnet/azurerm"
+module "network" {
+  source  = "app.terraform.io/cloud-castles/network/azurerm"
   version = "1.0.0"
-  resource_group = local.vnet_settings.resource_group
-  location = local.vnet_settings.location
-  vnet_name = local.vnet_settings.vnet_name
-  address_space = local.vnet_settings.address_space
+  address_space = local.netowrk_settings.address_space
+  location = local.netowrk_settings.location
+  resource_group = local.netowrk_settings.resource_group
+  subnet_objects = local.netowrk_settings.subnet_objects
+  vnet_name = local.netowrk_settings.vnet_name
 }
-
