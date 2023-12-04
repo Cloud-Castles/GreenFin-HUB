9c32d3e96d0936a769e3508bdb92836dec5a3635
David Schick <alufdavid@gmail.com>
2023-12-04 10:04:07 +0200

    test var

--- Network/main.tf
+++ Network/main.tf
@@ -4,12 +4,11 @@ locals {
 
 module "network" {
   source  = "app.terraform.io/cloud-castles/network/azurerm"
-  version = "1.0.2"
+  version = "1.0.3"
   address_space = local.netowrk_settings.address_space
   location = local.netowrk_settings.location
   resource_group = local.netowrk_settings.resource_group
   subnet_objects = local.netowrk_settings.subnet_objects
   vnet_name = local.netowrk_settings.vnet_name
+  env = var.env
 }
-
-#test_name
