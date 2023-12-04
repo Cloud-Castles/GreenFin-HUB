5d9aca446316583791788bdb0a7a69c0d4e7dd1c
David Schick <alufdavid@gmail.com>
2023-12-04 08:21:20 +0200

    change module name to network

--- Vnet/outputs.tf
+++ Vnet/outputs.tf
@@ -1,9 +1,9 @@
 output "vnet_name" {
   description = "ID of the created vnet"
-  value = module.vnet.vnet_name
+  value = module.network.vnet_name
 }
 
 output "subnet_ids" {
   description = "List of subnet id's"
-  value = module.vnet.subnet_ids
+  value = module.network.subnet_ids
 }
