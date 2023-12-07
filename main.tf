b3b5b69b85ccce8d3b858221eedac462c09bbb81
David Schick <alufdavid@gmail.com>
2023-12-07 13:19:15 +0200

    change structure

--- main.tf
+++ main.tf
@@ -1,23 +1,31 @@
 locals {
+  foundation_settings = jsondecode(file("Foundation/foundation_configuration.json"))
   netowrk_settings = jsondecode(file("Network/network_configuration.json"))
-  fw_settings = jsondecode(file("FW/fw_configuration.json"))
+  fw_settings = jsondecode(file("Resources/FW/fw_configuration.json"))
 }
+
+module "foundation" {
+  source  = "app.terraform.io/cloud-castles/foundation/azurerm"
+  version = "1.0.0"
+  resource_group = local.foundation_settings.resource_group
+  location = local.foundation_settings.location
+} 
 
 module "network" {
   source  = "app.terraform.io/cloud-castles/network/azurerm"
   version = "1.0.5"
-  address_space = local.netowrk_settings.address_space
-  location = local.netowrk_settings.location
-  resource_group = local.netowrk_settings.resource_group
-  subnet_objects = local.netowrk_settings.subnet_objects
+  resource_group = local.foundation_settings.resource_group
+  location = local.foundation_settings.location
   vnet_name = local.netowrk_settings.vnet_name
+  address_space = local.netowrk_settings.address_space
+  subnet_objects = local.netowrk_settings.subnet_objects
 } 
 
 module "fw" {
   source  = "app.terraform.io/cloud-castles/fw/azurerm"
   version = "1.0.7"
-  location = local.netowrk_settings.location
-  resource_group = local.netowrk_settings.resource_group
+  resource_group = local.foundation_settings.resource_group
+  location = local.foundation_settings.location
   subnet_id = element(module.network.subnet_ids, 0)
   pip_name = local.fw_settings.pip_name
   allocation_method = local.fw_settings.allocation_method
