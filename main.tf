0af40b73ff013c9e36315c7881ff7c7d180f5de5
David Schick <alufdavid@gmail.com>
2023-12-11 15:37:33 +0200

    poc with fw

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 locals {
   foundation_settings = jsondecode(file("Foundation/foundation_configuration.json"))
   netowrk_settings    = jsondecode(file("Network/network_configuration.json"))
-  # fw_settings = jsondecode(file("Resources/FW/fw_configuration.json"))
+  fw_settings         = jsondecode(file("Resources/FW/fw_configuration.json"))
 }
 
 module "foundation" {
@@ -13,7 +13,7 @@ module "foundation" {
 
 module "network" {
   source                = "app.terraform.io/cloud-castles/network/azurerm"
-  version               = "1.1.1"
+  version               = "1.1.2"
   resource_group        = local.foundation_settings.resource_group
   location              = local.foundation_settings.location
   vnet_name             = local.netowrk_settings.vnet_name
@@ -26,46 +26,36 @@ module "network" {
   depends_on = [module.foundation]
 }
 
+module "fw" {
+  source            = "app.terraform.io/cloud-castles/fw/azurerm"
+  version           = "1.0.8"
+  resource_group    = local.foundation_settings.resource_group
+  location          = local.foundation_settings.location
+  subnet_id         = lookup(module.network.subnet_ids, "AzureFirewallSubnet", null)
+  dns_servers       = local.netowrk_settings.dns_servers
+  pip_name          = local.fw_settings.pip_name
+  allocation_method = local.fw_settings.allocation_method
+  pip_sku           = local.fw_settings.pip_sku
+  fw_policy_name    = local.fw_settings.fw_policy_name
+  fw_policy_sku     = local.fw_settings.fw_policy_sku
+  fw_name           = local.fw_settings.fw_name
+  fw_sku            = local.fw_settings.fw_sku
+  fw_tier           = local.fw_settings.fw_tier
+  ip_conf_name      = local.fw_settings.ip_conf_name
 
-
-
-
-
-
-
-
+  depends_on = [module.network]
+}
 
 
 # module "pip" {
 #   source  = "app.terraform.io/cloud-castles/pip/azurerm"
 #   version = "1.0.0"
-#   pip_name = local.fw_settings.pip_name
-#   resource_group = local.foundation_settings.resource_group
-#   location = local.foundation_settings.location
-#   allocation_method = local.fw_settings.allocation_method
-#   pip_sku = local.fw_settings.pip_sku
+
 
 #   depends_on = [module.network]
 # }
 
-# module "fw" {
-#   source  = "app.terraform.io/cloud-castles/fw/azurerm"
-#   version = "1.0.7"
-#   resource_group = local.foundation_settings.resource_group
-#   location = local.foundation_settings.location
-#   subnet_id = element(module.network.subnet_ids, 0)
-#   pip_name = local.fw_settings.pip_name
-#   allocation_method = local.fw_settings.allocation_method
-#   pip_sku = local.fw_settings.pip_sku
-#   fw_policy_name = local.fw_settings.fw_policy_name
-#   fw_policy_sku = local.fw_settings.fw_policy_sku
-#   fw_name = local.fw_settings.fw_name
-#   fw_sku = local.fw_settings.fw_sku
-#   fw_tier = local.fw_settings.fw_tier
-#   ip_conf_name = local.fw_settings.ip_conf_name
 
-#   depends_on = [module.pip]
-# }
 
 # module "vpngw" {
 #   source  = "app.terraform.io/cloud-castles/fw/azurerm"
