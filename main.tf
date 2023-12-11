8414f890cfb70be6f073999bfa8a3e78fe9e9d96
David Schick <alufdavid@gmail.com>
2023-12-11 12:23:50 +0200

    add route tables to network

--- main.tf
+++ main.tf
@@ -11,6 +11,13 @@ module "foundation" {
   location = local.foundation_settings.location
 } 
 
+# module "route_table" {
+#   source  = "app.terraform.io/cloud-castles/foundation/azurerm"
+#   version = "1.0.0"
+#   resource_group = local.foundation_settings.resource_group
+#   location = local.foundation_settings.location
+# }
+
 module "network" {
   source  = "app.terraform.io/cloud-castles/network/azurerm"
   version = "1.0.5"
@@ -19,7 +26,19 @@ module "network" {
   vnet_name = local.netowrk_settings.vnet_name
   address_space = local.netowrk_settings.address_space
   subnet_objects = local.netowrk_settings.subnet_objects
-} 
+}
+
+module "pip" {
+  source  = "app.terraform.io/cloud-castles/pip/azurerm"
+  version = "1.0.0"
+  pip_name = local.fw_settings.pip_name
+  resource_group = local.foundation_settings.resource_group
+  location = local.foundation_settings.location
+  allocation_method = local.fw_settings.allocation_method
+  pip_sku = local.fw_settings.pip_sku
+
+  depends_on = [module.network]
+}
 
 module "fw" {
   source  = "app.terraform.io/cloud-castles/fw/azurerm"
@@ -37,8 +56,18 @@ module "fw" {
   fw_tier = local.fw_settings.fw_tier
   ip_conf_name = local.fw_settings.ip_conf_name
 
-  depends_on = [module.network]
+  depends_on = [module.pip]
 }
+
+module "vpngw" {
+  source  = "app.terraform.io/cloud-castles/fw/azurerm"
+  version = "1.0.0"
+  resource_group = local.foundation_settings.resource_group
+  location = local.foundation_settings.location
+
+  depends_on = [module.pip]
+}
+
 
 
 # module "storage_account" {
