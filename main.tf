67699c8bb723db632a0b457eb7abb55e6d364c3e
David Schick <alufdavid@gmail.com>
2023-12-18 18:53:09 +0200

    check CAC with new module of foundation

--- main.tf
+++ main.tf
@@ -1,51 +1,55 @@
 locals {
   foundation_settings = jsondecode(file("Foundation/foundation_configuration.json"))
-  netowrk_settings    = jsondecode(file("Network/network_configuration.json"))
-  fw_settings         = jsondecode(file("Resources/FW/fw_configuration.json"))
+  # netowrk_settings    = jsondecode(file("Network/network_configuration.json"))
+  # fw_settings         = jsondecode(file("Resources/FW/fw_configuration.json"))
   # vpngw_settings      = jsondecode(file("Resources/VPNGW/vpngw_configuration.json"))
 }
 
 module "foundation" {
   source         = "app.terraform.io/cloud-castles/foundation/azurerm"
-  version        = "1.0.0"
-  resource_group = local.foundation_settings.resource_group
-  location       = local.foundation_settings.location
+  version        = "1.0.1"
+  for_each = {
+    for name, group in local.foundation_settings.resource_groups :
+    name => group
+  }
+  name                  = each.key
+  location              = each.value.location
 }
 
-module "network" {
-  source                = "app.terraform.io/cloud-castles/network/azurerm"
-  version               = "1.1.2"
-  resource_group        = local.foundation_settings.resource_group
-  location              = local.foundation_settings.location
-  vnet_name             = local.netowrk_settings.vnet_name
-  address_space         = local.netowrk_settings.address_space
-  subnets               = local.netowrk_settings.subnets
-  fw_private_ip_address = local.netowrk_settings.fw_private_ip_address
-  dns_servers           = local.netowrk_settings.dns_servers
-  vpngw_rt_routes       = local.netowrk_settings.vpngw_rt_routes
+# module "network" {
+#   source                = "app.terraform.io/cloud-castles/network/azurerm"
+#   version               = "1.1.2"
+#   resource_group        = local.foundation_settings.resource_group
+#   location              = local.foundation_settings.location
+#   vnet_name             = local.netowrk_settings.vnet_name
+#   address_space         = local.netowrk_settings.address_space
+#   subnets               = local.netowrk_settings.subnets
+#   fw_private_ip_address = local.netowrk_settings.fw_private_ip_address
+#   dns_servers           = local.netowrk_settings.dns_servers
+#   vpngw_rt_routes       = local.netowrk_settings.vpngw_rt_routes
 
-  depends_on = [module.foundation]
-}
+#   depends_on = [module.foundation]
+# }
 
-module "fw" {
-  source            = "app.terraform.io/cloud-castles/fw/azurerm"
-  version           = "1.0.9"
-  resource_group    = local.foundation_settings.resource_group
-  location          = local.foundation_settings.location
-  subnet_id         = element(module.network.subnet_ids, 0)
-  dns_servers       = local.netowrk_settings.dns_servers
-  pip_name          = local.fw_settings.pip_name
-  allocation_method = local.fw_settings.allocation_method
-  pip_sku           = local.fw_settings.pip_sku
-  fw_policy_name    = local.fw_settings.fw_policy_name
-  fw_policy_sku     = local.fw_settings.fw_policy_sku
-  fw_name           = local.fw_settings.fw_name
-  fw_sku            = local.fw_settings.fw_sku
-  fw_tier           = local.fw_settings.fw_tier
-  ip_conf_name      = local.fw_settings.ip_conf_name
+# module "fw" {
+#   source            = "app.terraform.io/cloud-castles/fw/azurerm"
+#   version           = "1.0.9"
+#   resource_group    = local.foundation_settings.resource_group
+#   location          = local.foundation_settings.location
+#   subnet_id         = element(module.network.subnet_ids, 0)
+#   dns_servers       = local.netowrk_settings.dns_servers
+#   pip_name          = local.fw_settings.pip_name
+#   allocation_method = local.fw_settings.allocation_method
+#   pip_sku           = local.fw_settings.pip_sku
+#   fw_policy_name    = local.fw_settings.fw_policy_name
+#   fw_policy_sku     = local.fw_settings.fw_policy_sku
+#   fw_name           = local.fw_settings.fw_name
+#   fw_sku            = local.fw_settings.fw_sku
+#   fw_tier           = local.fw_settings.fw_tier
+#   ip_conf_name      = local.fw_settings.ip_conf_name
 
-  depends_on = [module.network]
-}
+#   depends_on = [module.network]
+# }
 
 # module "vpngw" {
 #   source                  = "app.terraform.io/cloud-castles/vpngw/azurerm"
--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
-output "vnet_name" {
-  description = "ID of the created vnet"
-  value = module.network.vnet_name
-}
+# output "vnet_name" {
+#   description = "ID of the created vnet"
+#   value = module.network.vnet_name
+# }
 
-output "subnet_ids" {
-  description = "List of subnet id's"
-  value = module.network.subnet_ids
-}
+# output "subnet_ids" {
+#   description = "List of subnet id's"
+#   value = module.network.subnet_ids
+# }
