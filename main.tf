82c0ea4a9c3d2754f05ff36c41be9b439338b80d
David Schick <alufdavid@gmail.com>
2023-12-20 10:43:05 +0200

    test new module

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ module "foundation" {
 
 module "network" {
   source                = "app.terraform.io/cloud-castles/network/azurerm"
-  version               = "1.1.4"
+  version               = "1.1.5"
   for_each = {
     for key, value in local.network_settings.vnets :
     key => value
