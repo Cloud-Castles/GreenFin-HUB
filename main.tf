3d0f41a60cc59af7a24f734a06814b677e7b1359
David Schick <alufdavid@gmail.com>
2023-12-20 11:52:20 +0200

    test new module

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ module "network" {
   source                = "app.terraform.io/cloud-castles/network/azurerm"
   version               = "1.1.8"
   ###########################################################
-  resource_group = local.dev_hub_rg
+  resource_group = "dev_hub_rg"
   location       = local.dev_hub_rg.location
   ###########################################################
   for_each = {
