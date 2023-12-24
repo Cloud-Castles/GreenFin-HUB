5937ffdb3507f1730432a3805cbd6a61f8f6938c
David Schick <alufdavid@gmail.com>
2023-12-24 16:42:51 +0200

    revert CAC to work with older module where the loop is in the main.tf of the CAC with version number test 13

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ module "foundation" {
 
 module "network" {
   source                = "app.terraform.io/cloud-castles/network/azurerm"
-  version               = "1.1.9"
+  version               = "1.2.1"
   for_each = {
     for key, value in local.network_settings.vnets :
     key => value
