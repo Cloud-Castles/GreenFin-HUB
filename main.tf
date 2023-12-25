90a130ede12e4647e383e887eb7b1fee7a7182ff
David Schick <alufdavid@gmail.com>
2023-12-25 15:28:32 +0200

    revert CAC to work with older module where the loop is in the main.tf of the CAC with version number test 29

--- main.tf
+++ main.tf
@@ -61,7 +61,7 @@ module "fw" {
   resource_group = module.foundation[each.value.targetResourceGroup].resource_group_name
   location       = module.foundation[each.value.targetResourceGroup].resource_group_location
   subnet_id      = module.network[each.value.targetSubnet].subnet_ids
-  dns_servers    = module.network[each.value.dns_servers]
+  dns_servers    = module.network[each.value.dns_servers].dns_servers
   ###########################################################
 
   depends_on = [module.network]
